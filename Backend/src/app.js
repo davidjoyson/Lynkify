e6671202e74a8719731329606ff19fb8e2533444
David Joyson <david.joyson@gmail.com>
2025-03-14 22:42:03 +0530

    server update

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,13 +1,9 @@
 import express from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
-import connectDB from './config/db.js';
 import authRoutes from './routes/authRoutes.js';
 
 const app = express();
-
-// Connect to MongoDB
-connectDB();
 
 // Middleware
 app.use(express.json());
